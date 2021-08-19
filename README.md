c939c950e328a851f9efcd4b48390f9a5274b8d7
Athul Prakash NJ <pschosherlock@gmail.com>
2021-08-19 21:18:15 +0530

    Added audio support 😆

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Just a simple telegram bot, that would convert an given image to text
+`https://www.pythonanywhere.com/forums/topic/10941/`
+`https://www.codegrepper.com/code-examples/python/python+convert+any+audio+to+wav`
--- main.py
+++ main.py
@@ -4,22 +4,49 @@ import pytesseract
 from PIL import Image
 from random import randint
 import logging
+from os import remove as delete
+from pydub import AudioSegment
+import speech_recognition as sr
 
+def generateRandom():
+    return random.randint(100, 9999999)
+
+r = sr.Recognizer()
 class textRactor:
-    def saveImage(self, image, name):
-        randomNumber = random.randint(100, 9999999)
-        fileName = f"images/{name}_{randomNumber}.jpg"
+    def saveFile(self, data, name, fType):
 
-        with open(fileName, "wb") as img:
-            img.write(image)
-        return fileName
+        randomNumber = generateRandom()
+        if fType=="image":
+            fileName = f"images/{name}_{randomNumber}.jpg"
+            with open(fileName, "wb") as img:
+                img.write(data)
+            return fileName
 
 
     def extractText(self, imagePath):
         img = Image.open(imagePath)
         imageData = pytesseract.image_to_string(img)
         return imageData
-
+    
+    def saveAudio(self, audio, name):
+        randomNumber = generateRandom()
+        fileName = f"audios/{name}_{randomNumber}.jpg"
+    
+    def recogniseOgg(self,data, name):
+        output = "test.wav"
+        randomNumber = generateRandom()
+        fileName = f"audios/{name}_{randomNumber}.ogg"
+        with open(fileName, 'wb') as ogg:
+            ogg.write(data)
+        
+        dest = fileName + '.wav'
+        sound = AudioSegment.from_ogg(fileName).export(dest, format="wav")
+        
+        with sr.AudioFile(dest) as source:
+            audio_data = r.record(source)
+            textData = r.recognize_google(audio_data)
+        delete(fileName)
+        return textData, dest
 
 with open('api.key') as f:
     apiKey = f.read();
@@ -38,6 +65,8 @@ def greet(message):
     Hello {message.from_user.first_name}
     """)
 
+tr = textRactor()
+
 @bot.message_handler(content_types=['photo']) # Filters By photo messages
 def photo(message):
 
@@ -50,9 +79,9 @@ def photo(message):
 
     bot.reply_to(message, f"Hmm..🕵️‍♂️ Let me read your image, {user} This might take a second or more ⏳")
 
-    tr = textRactor()
-    imageFile = tr.saveImage(image, user)
+    imageFile = tr.saveFile(image, user, 'image')
     imageData = tr.extractText(imageFile)
+    delete(imageFile) # Delete the file recieved (I consider my users privacy important)
     bot.send_message(message.chat.id, '😊 Here you go...')
     try:
         bot.reply_to(message, imageData)
@@ -60,4 +89,21 @@ def photo(message):
         imageData = "😖 Whoops, 🤷‍♂️ looks like the image doesn't contain any extractable text. Sorry 😬"
         bot.reply_to(message, imageData)
 
-bot.polling()
+
+
+@bot.message_handler(content_types=['voice'])
+def voice(message):
+    user = message.from_user.first_name
+    fileID = message.voice.file_id
+    file_info = bot.get_file(fileID)
+    voice = bot.download_file(file_info.file_path) # Downloads the file
+    logging.warning(f"{user} send a voice")
+    bot.reply_to(message, "🤨 What did you say?👂 Lemme listen carefully")
+    
+    converted, dest = tr.recogniseOgg(voice, user)
+    bot.reply_to(message, converted)
+
+    delete(dest)
+
+if __name__ == '__main__':
+    bot.polling()
